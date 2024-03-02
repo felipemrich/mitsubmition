3a18b179b1e49999fd3a28f7f99e9375e05f4c90
Dr. Emrich <felipeemrichdearaujo@gmail.com>
2024-03-01 21:20:26 -0300

    Third commit

--- server/dal.js
+++ server/dal.js
@@ -1,7 +1,7 @@
 import { MongoClient } from "mongodb";
 
 const url =
-  "MONGODB_URI=mongodb+srv://felipeemrichdearaujo:dwWlvVBNjBVDISHg@cluster0.wdpeh8b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+  "mongodb+srv://felipeemrichdearaujo:dwWlvVBNjBVDISHg@cluster0.wdpeh8b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 let db = null;
 
 // Function to establish a database connection
