cdea6d124cdedf938c2bb9575f47b01ac653f5a9
Dr. Emrich <felipeemrichdearaujo@gmail.com>
2024-03-01 21:34:46 -0300

    seventh commit

--- server/index.js
+++ server/index.js
@@ -5,11 +5,9 @@ import { fileURLToPath } from "url";
 import fs from "fs";
 import swaggerUi from "swagger-ui-express";
 import YAML from "yamljs";
-import dotenv from "dotenv";
 import accountRoutes from "./routes/accountRoutes.js";
 
 // Load environment variables from .env file
-dotenv.config();
 
 // Resolve directory name in ES module scope
 const __filename = fileURLToPath(import.meta.url);
@@ -21,7 +19,7 @@ const swaggerDocument = YAML.parse(
 );
 
 const app = express();
-const port_server = process.env.PORT || 4000; // Default to port 3000 if not specified
+const port_server = 4000; // Default to port 3000 if not specified
 
 // Adjust static middleware to serve from the correct path
 app.use(express.static(join(__dirname, "build")));
